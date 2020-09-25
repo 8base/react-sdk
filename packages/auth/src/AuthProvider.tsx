d072d45c7970cd304620a7947a9e8ce963b23b31
ej9x <strcmp9@gmail.com>
2020-09-25 15:01:48 +0300

    feat: add isEmailVerified data

--- packages/auth/src/AuthProvider.tsx
+++ packages/auth/src/AuthProvider.tsx
@@ -11,6 +11,7 @@ type AuthProviderProps = {
 
 type AuthProviderState = {
   isAuthorized: boolean;
+  isEmailVerified?: boolean;
   authState: IAuthState;
 };
 
@@ -54,8 +55,9 @@ class AuthProvider extends React.Component<AuthProviderProps, AuthProviderState>
 
     const authState = authClient.getState();
     const isAuthorized = authClient.checkIsAuthorized();
+    const isEmailVerified = authClient.checkIsEmailVerified && authClient.checkIsEmailVerified();
 
-    this.setState({ isAuthorized, authState });
+    this.setState({ isAuthorized, isEmailVerified, authState });
   };
 
   public componentDidMount() {
@@ -82,7 +84,7 @@ class AuthProvider extends React.Component<AuthProviderProps, AuthProviderState>
 
   public render() {
     const { children, authClient } = this.props;
-    const { isAuthorized, authState } = this.state;
+    const { isAuthorized, isEmailVerified, authState } = this.state;
 
     return (
       <AuthContext.Provider
@@ -90,6 +92,7 @@ class AuthProvider extends React.Component<AuthProviderProps, AuthProviderState>
           authClient,
           authState,
           isAuthorized,
+          isEmailVerified,
         }}
       >
         {children}
