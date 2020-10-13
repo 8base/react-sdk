da36b04a2aca126b88e7067d349b5452ad995ed0
Osipov Vladimir <zouxuoz@gmail.com>
2020-10-13 17:00:35 +0300

    fix(forms): export FORM_ERROR

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -4,3 +4,4 @@ export { Fieldset } from './Fieldset';
 export { FieldArray } from './FieldArray';
 export { FormContext } from './FormContext';
 export { FormSpy } from 'react-final-form';
+export { FORM_ERROR } from 'final-form';
