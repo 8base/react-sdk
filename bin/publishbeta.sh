9f1b53fd3b88749f432e71c786e4188cd3bc9ca6
Oscar Corcho <oscar.corcho@8base.com>
2024-02-23 20:38:59 -0500

    chore(update): updating beta publish

--- bin/publishbeta.sh
+++ bin/publishbeta.sh
@@ -2,14 +2,14 @@
 
 status=0
 
-(./bin/try-publish-beta.sh "app-provider") || status=1
-(./bin/try-publish-beta.sh "auth") || status=1
-(./bin/try-publish-beta.sh "crud") || status=1
-(./bin/try-publish-beta.sh "file-input") || status=1
-(./bin/try-publish-beta.sh "forms") || status=1
-(./bin/try-publish-beta.sh "permissions-provider") || status=1
-(./bin/try-publish-beta.sh "table-schema-provider") || status=1
-(./bin/try-publish-beta.sh "utils") || status=1
-(./bin/try-publish-beta.sh "8base-react-sdk") || status=1
+(./bin/try-publish.sh "app-provider") || status=1
+(./bin/try-publish.sh "auth") || status=1
+(./bin/try-publish.sh "crud") || status=1
+(./bin/try-publish.sh "file-input") || status=1
+(./bin/try-publish.sh "forms") || status=1
+(./bin/try-publish.sh "permissions-provider") || status=1
+(./bin/try-publish.sh "table-schema-provider") || status=1
+(./bin/try-publish.sh "utils") || status=1
+(./bin/try-publish.sh "8base-react-sdk") || status=1
 
 exit $status
